30dac808964f19a8fe1fde7f6d9a3542e82a8bf6
jvgiv <jvgiv12@gmail.com>
2019-11-01 15:15:44 -0400

    Router added, Layout created

---
+++ src/components/Mlb.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Mlb extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/components/SportPicker.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SportPicker extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
