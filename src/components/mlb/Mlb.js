4ac82ab9a12701f0c1741e1fa0c20728bb428811
John Gibbons <jvgiv12@gmail.com>
2020-10-08 15:19:58 -0400

    Displayed scores and team abbreviations after separating parntobject

--- src/components/mlb/Mlb.js
+++ src/components/mlb/Mlb.js
@@ -7,7 +7,11 @@ export default class Mlb extends Component {
         super(props)
     
         this.state = {
-             gameStats: {}
+            gameStats: {},
+            awayScores: [],
+            homeScores: [],
+            awayInfo: {},
+            homeInfo: {}
         }
     }
 
@@ -20,7 +24,11 @@ export default class Mlb extends Component {
             .get('https://chumley.barstoolsports.com/dev/data/games/eed38457-db28-4658-ae4f-4d4d38e9e212.json')
             .then(res => {
                 this.setState({
-                    gameStats: res.data
+                    gameStats: res.data,
+                    awayScores: res.data.away_period_scores,
+                    homeScores: res.data.home_period_scores,
+                    awayInfo: res.data.away_team,
+                    homeInfo: res.data.home_team
                 })
             })
             .catch(err => console.log(err.message))
@@ -28,10 +36,16 @@ export default class Mlb extends Component {
     
     
     render() {
-        console.log(this.state.gameStats)
+        console.log(this.state.homeInfo)
         return (
             <div>
-                
+                <Scoreboard 
+                    info={this.state.gameStats} 
+                    awayScores={this.state.awayScores}
+                    homeScores={this.state.homeScores}
+                    homeInfo={this.state.homeInfo}
+                    awayInfo={this.state.awayInfo}    
+                />
             </div>
         )
     }
